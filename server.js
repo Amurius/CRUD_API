b6228095f0955a9a20428c8d356a88807ef7d6cd
Amurius <artuslr78@gmail.com>
2024-04-18 16:04:32 +0200

    back

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const  express = require('express');
 const cors = require('cors');
 const app = express();
 const celebrites = require('./celebrities.json');
+const fs = require('fs');
 
 app.use(cors());
 app.use(express.json())
 
-.post('/', (req, res) => {
+.get('/', (req, res) => {
   console.log(req.body);
   celebrites.push(req.body)
   res.status(200).json(celebrites)
+})
+.post('/Ajout', (req, res) => {
+  const celebrite = [
+    {nom: req.body.nom,  prenom: req.body.prenom, age: req.body.age}
+  ];
+  const celebrites = celebrites.push(celebrite)
+  fs.writeFileSync('./celebrities.json', JSON.stringify(celebrites,null,2))
 })
 
 .listen(3000, () => {
